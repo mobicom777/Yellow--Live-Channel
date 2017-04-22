3822f49f780db4402b91fe9c58e5b421e3e432f2
mobicom777 <j.mobicom@gmail.com>
2017-04-22 13:55:18 +1000

    final stream

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var myOptions = {
 myPlayer = amp("azuremediaplayer", myOptions);
 myPlayer.src([
         {
-                "src": "http://mobicom.streaming.mediaservices.windows.net/7af2ac44-27ea-4382-b689-aca1b24049e4/1f4a1421-162e-44df-b112-10adddb19219.ism/manifest",
+                "src": "http://mobicom.streaming.mediaservices.windows.net/37b5ef05-174e-44c7-afe7-1e24e7ac8da7/c6d4bfcc-7fb3-4697-b418-7a44daf250b6.ism/manifest",
                 "type": "application/vnd.ms-sstr+xml"
         }
 ]);
