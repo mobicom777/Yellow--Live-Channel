dbd67958d0d36a53c0e6de4b85cc60001b970103
mobicom777 <j.mobicom@gmail.com>
2017-04-22 13:47:09 +1000

    mobicom stream update with http

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var myOptions = {
 myPlayer = amp("azuremediaplayer", myOptions);
 myPlayer.src([
         {
-                "src": "//mobicom.streaming.mediaservices.windows.net/7af2ac44-27ea-4382-b689-aca1b24049e4/1f4a1421-162e-44df-b112-10adddb19219.ism/manifest",
+                "src": "http://mobicom.streaming.mediaservices.windows.net/7af2ac44-27ea-4382-b689-aca1b24049e4/1f4a1421-162e-44df-b112-10adddb19219.ism/manifest",
                 "type": "application/vnd.ms-sstr+xml"
         }
 ]);
