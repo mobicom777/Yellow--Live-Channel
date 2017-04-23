d238b40c4c8599317dfe8c7c059fab0ace7f539f
mobicom777 <j.mobicom@gmail.com>
2017-04-23 15:21:17 +1000

    1080p

--- main.js
+++ main.js
@@ -9,5 +9,5 @@ var myOptions = {
 myPlayer = amp("azuremediaplayer", myOptions);
 
 myPlayer.src([
-    { src: "http://mobicom.streaming.mediaservices.windows.net/058fe846-0937-4f8d-89eb-fb43511d20d8/Sri Lanka - by drone 2017 - 4K.ism/manifest", type: "application/vnd.ms-sstr+xml", streamingFormats: ["SMOOTH", "HLS","HLS-V3", "HLS-V4"]}, 
+    { src: "http://mobicom.streaming.mediaservices.windows.net/900a3e1b-f403-4042-9c5d-c238049c9444/Best Places to Visit in Sri Lanka, GoPlaces Sri Lanka  8 Wonders.ism/manifest", type: "application/vnd.ms-sstr+xml", streamingFormats: ["SMOOTH", "HLS","HLS-V3", "HLS-V4"]}, 
 ]);
