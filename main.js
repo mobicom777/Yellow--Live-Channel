8c810a74f87ba47a168d4799b0eae8f53c7248d8
mobicom777 <j.mobicom@gmail.com>
2017-04-23 20:01:41 +1000

    liveTv

--- main.js
+++ main.js
@@ -9,5 +9,5 @@ var myOptions = {
 myPlayer = amp("azuremediaplayer", myOptions);
 
 myPlayer.src([
-    { src: "http://mobicom.streaming.mediaservices.windows.net/900a3e1b-f403-4042-9c5d-c238049c9444/Best Places to Visit in Sri Lanka, GoPlaces Sri Lanka  8 Wonders.ism/manifest", type: "application/vnd.ms-sstr+xml", streamingFormats: ["SMOOTH", "HLS","HLS-V3", "HLS-V4"]}, 
+    { src: "http://mobicom.streaming.mediaservices.windows.net/15fa66fe-250d-4f69-8be3-85f37f7e4942/c228dc66-4ae9-432c-ad8d-60a465b10c37.ism/manifest", type: "application/vnd.ms-sstr+xml", streamingFormats: ["SMOOTH", "HLS","HLS-V3", "HLS-V4"]}, 
 ]);
