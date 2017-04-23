938ec693360e3b7b1e2ee49d0637222d91bc1e52
mobicom777 <j.mobicom@gmail.com>
2017-04-23 14:23:40 +1000

    update multi format

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 var myOptions = {
 	"nativeControlsForTouch": false,
 	"logo": { "enabled": false },
-	techOrder: ["azureHtml5JS", "html5", "flashSS", "silverlightSS", ],
 	controls: true,
 	autoplay: true,
 	width: "640",
 	height: "400",
 }
 myPlayer = amp("azuremediaplayer", myOptions);
+
 myPlayer.src([
-        {
-                "src": "http://mobicom.streaming.mediaservices.windows.net/4082234e-de8e-46f6-bff0-01df846795e8/b35e7f08-a06c-46dc-9a9a-504b4f1360d3.ism/manifest",
-                "type": "application/vnd.ms-sstr+xml"
-        }
+    { src: "//amssamples.streaming.mediaservices.windows.net/91492735-c523-432b-ba01-faba6c2206a2/AzureMediaServicesPromo.ism/manifest", type: "application/vnd.ms-sstr+xml", streamingFormats: ["SMOOTH", "HLS","HLS-V3", "HLS-V4"]}, 
 ]);
