010b383eac0609cdbd1582039b6506dac2a84583
mobicom777 <j.mobicom@gmail.com>
2017-04-29 20:26:36 +1000

    yellow channel Paya seetha

--- main.js
+++ main.js
@@ -15,5 +15,5 @@
 myPlayer = amp("azuremediaplayer", myOptions);
 
 myPlayer.src([
-    { src: "http://mobicom.streaming.mediaservices.windows.net/3d30369e-0658-4ff5-a79c-3659cde656a1/Kal Ho Naa Ho - Title Track Video  Shahrukh Khan, Saif, Preity.ism/manifest", type: "application/vnd.ms-sstr+xml", streamingFormats: ["SMOOTH", "HLS","HLS-V3", "HLS-V4"]}, 
+    { src: "http://mobicom.streaming.mediaservices.windows.net/3e7cfaf6-597d-405a-a29a-e2a5f69e1e2b/Paya Seetha Raye - Vijaya Kumarathunga.ism/manifest", type: "application/vnd.ms-sstr+xml", streamingFormats: ["SMOOTH", "HLS","HLS-V3", "HLS-V4"]}, 
 ]);
