57a78d8ef7039d96beccba37016d7ddb877a2536
mobicom777 <j.mobicom@gmail.com>
2017-04-30 21:15:56 +1000

    live passthrough

--- main.js
+++ main.js
@@ -15,5 +15,5 @@
 myPlayer = amp("azuremediaplayer", myOptions);
 
 myPlayer.src([
-    { src: "http://mobicom.streaming.mediaservices.windows.net/3e7cfaf6-597d-405a-a29a-e2a5f69e1e2b/Paya Seetha Raye - Vijaya Kumarathunga.ism/manifest", type: "application/vnd.ms-sstr+xml", streamingFormats: ["SMOOTH", "HLS","HLS-V3", "HLS-V4"]}, 
+    { src: "http://mobicom.streaming.mediaservices.windows.net/bc3f1b68-137e-40a3-8bbd-f1bd16cbdd17/f0c965ef-aa63-473b-96fd-71a1faca8a69.ism/manifest", type: "application/vnd.ms-sstr+xml", streamingFormats: ["SMOOTH", "HLS","HLS-V3", "HLS-V4"]}, 
 ]);
